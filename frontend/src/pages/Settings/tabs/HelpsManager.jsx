ef7d26cfd794f122380afcb7179cb073994cc9d2
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-05-31 18:40:44 -0300

    fix settings

--- frontend/src/pages/Settings/tabs/HelpsManager.jsx
+++ frontend/src/pages/Settings/tabs/HelpsManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useCallback, useMemo, useContext } from "react";
 import PropTypes from 'prop-types';
 import {
     TextField,
